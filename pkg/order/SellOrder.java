d3e72fe0efa8e253588ae5a8876c90645084792a
Ian <Typhoan@users.noreply.github.com>
2015-04-24 17:48:55 -0500

    Create SellOrder.java

---
+++ pkg/order/SellOrder.java
@@ -0,0 +1,39 @@
+package pkg.order;
+
+import pkg.exception.StockMarketExpection;
+import pkg.trader.Trader;
+
+public class SellOrder extends Order {
+	
+	public SellOrder(String stockSymbol, int size, double price, Trader trader) {
+		this.stockSymbol = stockSymbol;
+		this.size = size;
+		this.price = price;
+		this.trader = trader;
+	}
+
+	public SellOrder(String stockSymbol, int size, boolean isMarketOrder,
+			Trader trader) throws StockMarketExpection {
+		// Create a new sell order which is a market order
+		// Set the price to 0.0, Set isMarketOrder attribute to true
+		//
+		// If this is called with isMarketOrder == false, throw an exception
+		// that an order has been placed without a valid price.
+		
+		if (isMarketOrder) {
+			this.stockSymbol = stockSymbol;
+			this.size = size;
+			this.price = 0.0;
+			this.trader = trader;
+			this.isMarketOrder = true;
+		}
+		else {
+			throw new StockMarketExpection("Order was placed without a valid price");
+		}
+	}
+
+	public void printOrder() {
+		System.out.println("Stock: " + stockSymbol + " $" + price + " x "
+				+ size + " (Sell)");
+	}
+}
